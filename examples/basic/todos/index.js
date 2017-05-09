9358dde692877d73874f118e81c2221b324b2074
dmitriz <dmitri14@gmail.com>
2017-05-09 16:35:09 +0100

    add todos example

---
+++ examples/basic/todos/index.js
@@ -0,0 +1,60 @@
+// uncomponent - the pair of functions, 
+// no imports - no external dependencies!
+
+// compute new state
+const reducer = ({ submitted } = {}, title) => ({
+	title: ``,
+
+	// add new value to submitted array
+	submitted: submitted.concat([title]),
+	changed: true
+})
+
+const style = {width: '100%'}
+
+// pure with no dependencies
+const view = ({ form, input, p }) => 
+	({ title, submitted, changed }, dispatch) => [
+		`Enter your Todo: `,
+		form({
+			onsubmit: e => {
+				// prevents page reload
+				e.preventDefault()
+				dispatch(e.target.in.value)
+			}
+		}, [
+			input({
+				name: 'in',
+				style,
+				class: 'new-todo',
+				placeholder: 'Your next Todo...',
+				autofocus: true,
+				value: title
+			})
+		]),
+		changed 
+			? `Thank you, here are your Todos:`
+			: `You have no Todos`,
+		p({style: {
+			color: changed ? 'blue' : 'gray'
+		}}, submitted.map(key => p(` ${key} `)))
+	]
+
+
+// the only method we need
+const mount = require('./un-mount')
+
+// create dom element
+const el = document.createElement('div')
+document.body.appendChild(el)
+
+// mount our live uncomponent
+mount({ 
+	el, 
+	reducer, 
+	view, 
+	initState: {
+		submitted: [],
+		changed: false
+	}
+})
