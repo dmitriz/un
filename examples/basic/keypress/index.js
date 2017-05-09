0c94acdc3564b2b44709a015c44aa8a08f7484a5
dmitriz <dmitri14@gmail.com>
2017-05-09 07:57:15 +0100

    Start keypress example

---
+++ examples/basic/keypress/index.js
@@ -0,0 +1,56 @@
+// uncomponent - the pair of functions, 
+// no imports - no external dependencies!
+
+// simply return action's value as next state
+const reducer = ({current, changed}, value) => ({
+	current: value,
+	changed: true
+})
+
+const style = {width: '100%'}
+
+// pure with no dependencies
+const view = ({ form, input, p }) => ({ current, changed }, dispatch) => [
+	`Welcome to my Submit: `,
+	`Type and hit ENTER: `,
+	form({
+		onsubmit: e => {
+			// prevents page reload
+			e.preventDefault()
+			dispatch(e.target.in.value)
+		}
+	}, [
+		input({
+			name: 'in',
+			style,
+			class: 'new-todo',
+			placeholder: 'What is your string today?',
+			autofocus: true,
+		})
+	]),
+	changed 
+		? `Thank you, here is your submission: ` 
+		: `You have submitted: `,
+	p({style: {
+		color: changed ? 'blue' : 'gray'
+	}}, current)
+]
+
+
+// the only method we need
+const mount = require('./un-mount')
+
+// create dom element
+const el = document.createElement('div')
+document.body.appendChild(el)
+
+// mount our live uncomponent
+mount({ 
+	el, 
+	reducer, 
+	view, 
+	initState: {
+		current: `Nothing as of yet`,
+		changed: false
+	}
+})
