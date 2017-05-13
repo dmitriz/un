974f47cbc29ca6d66cac4814dc001eae4c3e8e19
dmitriz <dmitri14@gmail.com>
2017-05-13 07:05:23 +0100

    update descr

--- adapters/factory.js
+++ adapters/factory.js
@@ -1,6 +1,12 @@
 /**
- * Set prototype object for all instances created by the Factory
- * (can be used to provide additional instance methods)
+ * Transforms a _plain_ object creating Factory F to the new Factory T(F)
+ * creating the same plain objects with the additional specified prototype set on each object
+ * (can be used to provide additional instance methods on the object created)
+ *
+ * Warning. If the objects were created by F with prototypes, they will be overwritten by T(F).
+ * This is by design, to avoid performance overheads overusing prototype chains.
+ * In particular, this transform may not be suitable for Factories using constructors
+ * and relying on their prototypal chains.
  */ 
 const setInstanceProto = protoObj => Factory =>
   (...args) => {
