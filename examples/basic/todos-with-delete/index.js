0c5be98a7e04a4c91dec65f8853d850a9856cea4
Dmitri Zaitsev <dmitri14@gmail.com>
2017-06-05 20:57:57 +0100

    clean

--- examples/basic/todos-with-delete/index.js
+++ examples/basic/todos-with-delete/index.js
@@ -12,56 +12,48 @@ const reducer = (state = {}, { type, todo } = {}) => {
 		'DELETE': {
 			// delete by name!
 			todos: todos.filter(t => t !== todo)
-		}
+		},
 	}[type] || state
 }
 
 
 
 // pure with no dependencies
-const view = ({ div, header, form, input, label, p, ul, li, section, h1, button }) =>
+const view = ({ div, header, form, input, label, p, ul, li, h1, button }) =>
 	({ newTitle, todos }, dispatch) => {
 
-	const NewTodoInput = newTitle =>
-		header('.header', [
-			h1("todos"),
-			form({
-				onsubmit: e => {
-					e.preventDefault()
-          const todo = e.target.in.value.trim()
-					todo && dispatch({ type: 'ADD', todo })
-				}
-			}, [
-				input('#new-todo', {
-					name: 'in',
-					placeholder: 'What needs to be done?',
-					autofocus: true,
-					value: newTitle
-				})
-			])
+	const NewTodoInput = newTitle => [
+		h1("todos"),
+		form({
+			onsubmit: e => {
+				e.preventDefault()
+        const todo = e.target.in.value.trim()
+				todo && dispatch({ type: 'ADD', todo })
+			}
+		}, [
+			input('#new-todo', {
+				name: 'in',
+				placeholder: 'What needs to be done?',
+				autofocus: true,
+				value: newTitle,
+			})
 		])
+  ]
 
 	const Todo = todo =>
 		li([
-			div(".view", [
-				label(todo),
-				button(".destroy", {
-					onclick: () => dispatch({type: 'DELETE', todo})
-				}),
-			]),
+			label( todo ),
+			button(".destroy", {
+				onclick: () => dispatch({ type: 'DELETE', todo })
+			}),
 		])
 
 	const Main = todos =>
-		section("#main", {
-			style: {display: todos.length > 0 ? "" : "none"}
-		}, [
-			ul('#todo-list', todos.map(todo => Todo(todo)))
-		])
-
+		ul('#todo-list', todos.map(Todo))
 
 	return [
 		NewTodoInput( newTitle ),
-		Main( todos )
+		Main( todos ),
 	]
 }
 
